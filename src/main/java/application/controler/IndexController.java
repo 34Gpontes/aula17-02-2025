df722d7c61bd1b9a7519a00cbc78f13daf54ec02
34Gpontes <141674652+34Gpontes@users.noreply.github.com>
2025-02-17 17:56:59 +0000

    aula17-02-2025

---
+++ src/main/java/application/controler/IndexController.java
@@ -0,0 +1,17 @@
+package application.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String home (){
+        return "home - GET";
+    }
+    @PostMapping("/")
+    public String homePost(){
+        return "home - POST";
+    }
+}
